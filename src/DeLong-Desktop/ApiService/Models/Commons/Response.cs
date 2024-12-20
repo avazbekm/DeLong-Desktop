224deab65fcd61bd3ab65468e0f32be99588fed6
Avazbek0704 <siddikovavazbek213@gmail.com>
2024-12-20 10:14:22 +0500

    added Response

---
+++ src/DeLong-Desktop/ApiService/Models/Commons/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace DeLong_Desktop.ApiService.Models.Commons;
+
+class Response<T>
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T Data { get; set; }
+}
