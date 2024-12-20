fb691424cec79349dd2bbfb5858a2ccc4200a419
Avazbek0704 <siddikovavazbek213@gmail.com>
2024-12-20 15:14:31 +0500

    added customersPage and customerAddWindow

---
+++ src/DeLong-Desktop/Pages/Customers/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace DeLong_Desktop.Pages.Customers;
+
+class Item
+{
+    public int Id { get; set; }
+    public string FirmaName {  get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string TelegramPhone {  get; set; } = string.Empty;
+    public long JSHSHIR {  get; set; }
+    public string Adress {  get; set; } = string.Empty;
+}
---
+++ src/DeLong-Desktop/Windows/Customers/CustomerAddWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace DeLong_Desktop.Windows.Customers;
+
+/// <summary>
+/// Interaction logic for CustomerAddWindow.xaml
+/// </summary>
+public partial class CustomerAddWindow : Window
+{
+    public CustomerAddWindow()
+    {
+        InitializeComponent();
+    }
+}
