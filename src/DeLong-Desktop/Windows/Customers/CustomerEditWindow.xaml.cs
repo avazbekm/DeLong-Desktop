76849635d837116bd3a94a54e3247c3daa2631cf
Avazbek0704 <siddikovavazbek213@gmail.com>
2024-12-27 10:16:21 +0500

    added customerEditWindow

--- src/DeLong-Desktop/Windows/Customers/CustomerEditWindow.xaml.cs
+++ src/DeLong-Desktop/Windows/Customers/CustomerEditWindow.xaml.cs
@@ -1,18 +1,15 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using DeLong_Desktop.ApiService.DTOs.Enums;
-using DeLong_Desktop.ApiService.DTOs.Users;
 using DeLong_Desktop.ApiService.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-using DeLong_Desktop.ApiService.DTOs.Customers;
 
 namespace DeLong_Desktop.Windows.Customers;
 
 /// <summary>
-/// Interaction logic for CustomerAddWindow.xaml
+/// Interaction logic for CustomerEditWindow.xaml
 /// </summary>
-public partial class CustomerAddWindow : Window
+public partial class CustomerEditWindow : Window
 {
     private readonly IUserService userService;
     private readonly ICustomerService customerService;
@@ -21,103 +18,15 @@ public partial class CustomerAddWindow : Window
 
     string gender = "";
     string userJshshir = "";
-
-    public CustomerAddWindow(IServiceProvider services)
+    public CustomerEditWindow(IServiceProvider services)
     {
         InitializeComponent();
         this.services = services;
         this.userService = services.GetRequiredService<IUserService>();
         this.customerService = services.GetRequiredService<ICustomerService>();
     }
-      
-        // radio buttonlar
-    private void rbtnYurdik_Checked(object sender, RoutedEventArgs e)
-    {
-        spYurCutomer.Visibility = Visibility.Visible;
-        spJisCutomer.Visibility = Visibility.Hidden;
-        spYattCutomer.Visibility = Visibility.Hidden;
-        spYurYattJis.Visibility = Visibility.Visible;
-        spQaytish.Visibility = Visibility.Hidden;
-    }
-    private void rbtnYaTT_Checked(object sender, RoutedEventArgs e)
-    {
-        spYurCutomer.Visibility = Visibility.Hidden;
-        spJisCutomer.Visibility = Visibility.Hidden;
-        spYattCutomer.Visibility = Visibility.Visible;
-        spYurYattJis.Visibility = Visibility.Visible;
-        spQaytish.Visibility = Visibility.Hidden;
-    }
-    private void rbtnJismoniy_Checked(object sender, RoutedEventArgs e)
-    {
-        spYurCutomer.Visibility = Visibility.Hidden;
-        spJisCutomer.Visibility = Visibility.Visible;
-        spYattCutomer.Visibility = Visibility.Hidden;
-        spYurYattJis.Visibility = Visibility.Visible;
-        spQaytish.Visibility = Visibility.Hidden;
-    }
-    private void btnJisAdd_Click(object sender, RoutedEventArgs e)
-    {
-        UserCreationDto userCreationDto = new UserCreationDto();
 
-        userCreationDto.LastName = txtFamiliya.Text.ToLower();
-        userCreationDto.FirstName = txtIsmi.Text.ToLower();
-        userCreationDto.Patronomyc = txtSharifi.Text.ToLower();
-        userCreationDto.SeriaPasport = txtPasportSeria.Text.ToLower();
-        userCreationDto.Address = txtJisAdres.Text.ToLower();
-        userCreationDto.Phone = txtJisTelefon.Text;
-        userCreationDto.TelegramPhone = txtJisTelegramRaqam.Text;
-        userCreationDto.JSHSHIR = txtJisJSHSHIR.Text;
-        userJshshir = txtJisJSHSHIR.Text;
-
-
-        if (!dateOfBirthPicker.SelectedDate.HasValue)
-        {
-            MessageBox.Show("Tug'ulgan sanani kiriting iltimos.");
-            return;
-        }
-        userCreationDto.DateOfBirth = dateOfBirthPicker.SelectedDate.Value.ToUniversalTime();
-
-        if (!dateOfIssuePicker.SelectedDate.HasValue)
-        {
-            MessageBox.Show("Pasport berilgan sanani kiriting iltimos.");
-            return;
-        }
-
-        userCreationDto.DateOfIssue = dateOfIssuePicker.SelectedDate.Value.ToUniversalTime();
-        if (!dateOfExpiryPicker.SelectedDate.HasValue)
-        {
-            MessageBox.Show("Pasportni amal qilish sanani kiriting iltimos.");
-            return;
-        }
-        userCreationDto.DateOfExpiry = dateOfExpiryPicker.SelectedDate.Value.ToUniversalTime();
-        if (gender.Equals("Erkak"))
-            userCreationDto.Gender = (Gender)0;
-        else if (gender.Equals("Ayol"))
-            userCreationDto.Gender = (Gender)1;
-
-
-        if (userCreationDto.FirstName.Equals("") ||
-            userCreationDto.LastName.Equals("") ||
-            userCreationDto.SeriaPasport.Equals("") ||
-            userCreationDto.Address.Equals("") ||
-            userCreationDto.JSHSHIR.Equals("") ||
-            userCreationDto.TelegramPhone.Equals("") ||
-            userCreationDto.Phone.Equals(""))
-
-            MessageBox.Show("Malumotni to'liq kiriting!");
-        else
-        {
-            var result = this.userService.AddAsync(userCreationDto);
-
-            if (!result.IsCompletedSuccessfully)
-            {
-                MessageBox.Show($" Saqlandi.");
-                IsCreated = true;
-            }
-            else
-                MessageBox.Show($"{"Saqlashda xatolik"}");
-        }
-    }
+    // radio buttonlar
     private void RadioButton_Checked(object sender, RoutedEventArgs e)
     {
         RadioButton selectedRadioButton = sender as RadioButton;
@@ -132,212 +41,13 @@ public partial class CustomerAddWindow : Window
         }
 
     }
-        
-        // buttonlar 
-    private async void btnYurAdd_Click(object sender, RoutedEventArgs e)
-    {
-        CustomerCreationDto customerCreationDto = new CustomerCreationDto();
-
-        customerCreationDto.Name = txtYurNomi.Text.ToLower();
-        if (txtYurINN.Text.Length != 9)
-        {
-            MessageBox.Show("INN kiriting iltimos.");
-            return;
-        }
-        customerCreationDto.INN = int.Parse(txtYurINN.Text);
-        customerCreationDto.MFO = txtYurMFO.Text;
-
-        if (txtYurXisobRaqam.Text.Length != 23)
-        {
-            MessageBox.Show("Xisob raqamni to'liq kiriting iltimos.");
-            return;
-        }
-
-        customerCreationDto.BankAccount = RemoveDashes(txtYurXisobRaqam.Text);
-        customerCreationDto.BankName = txtYurBank.Text.ToLower();
-        customerCreationDto.OKONX = txtYurOKONX.Text;
-        customerCreationDto.YurAddress = txtYurFirmaAdres.Text.ToLower();
-
-        var user = await userService.RetrieveByJSHSHIRAsync(userJshshir);
-        if (user is null)
-        {
-            MessageBox.Show("Rahbar ma'lumotini kiriting.");
-            return;
-        }
-        customerCreationDto.UserId = user.Id;
-
-        if (customerCreationDto.Name.Equals("") ||
-           customerCreationDto.INN.Equals("") ||
-           customerCreationDto.MFO.Equals("") ||
-           customerCreationDto.BankName.Equals("") ||
-           customerCreationDto.OKONX.Equals("") ||
-           customerCreationDto.YurAddress.Equals(""))
-
-            MessageBox.Show("Ma'lumotni to'liq kiriting!");
-        else
-        {
-            var result = this.customerService.AddAsync(customerCreationDto);
-
-            if (!result.IsCompletedSuccessfully)
-            {
-                MessageBox.Show($" Saqlandi.");
-                IsCreated = true;
-            }
-            else
-                MessageBox.Show($"{"Saqlashda xatolik"}");
-        }
-
-    }
-    private void btnRahbar_Click(object sender, RoutedEventArgs e)
-    {
-        spYurYattJis.Visibility = Visibility.Hidden;
-        spYurCutomer.Visibility = Visibility.Hidden;
-        spJisCutomer.Visibility = Visibility.Hidden;
-        spYattCutomer.Visibility = Visibility.Hidden;
-        spQaytish.Visibility = Visibility.Visible;
-        spJisNew.Visibility = Visibility.Visible;
-    }
-    private void btnQaytish_Click(object sender, RoutedEventArgs e)
-    {
-        if (rbtnYurdik.IsChecked.Equals(true))
-        {
-            spYurCutomer.Visibility = Visibility.Visible;
-            spJisCutomer.Visibility = Visibility.Hidden;
-            spYattCutomer.Visibility = Visibility.Hidden;
-            spYurYattJis.Visibility = Visibility.Visible;
-            spQaytish.Visibility = Visibility.Hidden;
-            spJisNew.Visibility = Visibility.Hidden;
-        }
-        else if (rbtnYaTT.IsChecked.Equals(true))
-        {
-            spYurCutomer.Visibility = Visibility.Hidden;
-            spJisCutomer.Visibility = Visibility.Hidden;
-            spYattCutomer.Visibility = Visibility.Visible;
-            spYurYattJis.Visibility = Visibility.Visible;
-            spQaytish.Visibility = Visibility.Hidden;
-            spJisNew.Visibility = Visibility.Hidden;
-        }
-    }
-    private void btnNewAdd_Click(object sender, RoutedEventArgs e)
-    {
-        spYurCutomer.Visibility = Visibility.Hidden;
-        spJisCutomer.Visibility = Visibility.Visible;
-        spYattCutomer.Visibility = Visibility.Hidden;
-        spYurYattJis.Visibility = Visibility.Hidden;
-        spQaytish.Visibility = Visibility.Visible;
-        spJisNew.Visibility = Visibility.Hidden;
-    }
-    private async void btnSearch_Click(object sender, RoutedEventArgs e)
-    {
-        if (txtSearchJ.Text.Length != 14)
-        {
-            MessageBox.Show("JSHSHIRni to'liq kiriting iltimos.");
-            return;
-        }
-        var existUser = await userService.RetrieveByJSHSHIRAsync(txtSearchJ.Text);
-        if (existUser is not null)
-        {
-            userJshshir = existUser.JSHSHIR;
-            MessageBox.Show($"{txtSearchJ.Text} JSHSHIR li mijoz mavjud.");
-            btnExistClient.Visibility = Visibility.Visible;
-        }
-        else
-        {
-            MessageBox.Show($"{txtSearchJ.Text} JSHSHIR li mijoz mavjud emas.");
-            btnNewAdd.Visibility = Visibility.Visible;
-        }
-    }
-    private void btnExistClient_Click(object sender, RoutedEventArgs e)
-    {
-        if (rbtnYurdik.IsChecked.Equals(true))
-        {
-            spYurCutomer.Visibility = Visibility.Visible;
-            spJisCutomer.Visibility = Visibility.Hidden;
-            spYattCutomer.Visibility = Visibility.Hidden;
-            spYurYattJis.Visibility = Visibility.Visible;
-            spQaytish.Visibility = Visibility.Hidden;
-            spJisNew.Visibility = Visibility.Hidden;
-        }
-        else if (rbtnYaTT.IsChecked.Equals(true))
-        {
-            spYurCutomer.Visibility = Visibility.Hidden;
-            spJisCutomer.Visibility = Visibility.Hidden;
-            spYattCutomer.Visibility = Visibility.Visible;
-            spYurYattJis.Visibility = Visibility.Visible;
-            spQaytish.Visibility = Visibility.Hidden;
-            spJisNew.Visibility = Visibility.Hidden;
-        }
-    }
-    private void btnYaTTRahbar_Click(object sender, RoutedEventArgs e)
-    {
-
-        spYurYattJis.Visibility = Visibility.Hidden;
-        spYurCutomer.Visibility = Visibility.Hidden;
-        spJisCutomer.Visibility = Visibility.Hidden;
-        spYattCutomer.Visibility = Visibility.Hidden;
-        spQaytish.Visibility = Visibility.Visible;
-        spJisNew.Visibility = Visibility.Visible;
-    }
-    private async void btnYattAdd_Click(object sender, RoutedEventArgs e)
-    {
-        CustomerCreationDto customerCreationDto = new CustomerCreationDto();
-
-        customerCreationDto.Name = txtYattNomi.Text.ToLower();
-        if (txtYattJSHSHIR.Text.Length != 14)
-        {
-            MessageBox.Show("JSHSHIR ni to'liq kiriting iltimos.");
-            return;
-        }
-        customerCreationDto.JSHSHIR = txtYattJSHSHIR.Text;
-        customerCreationDto.MFO = txtYattMFO.Text;
-
-        if (txtYattXisobRaqam.Text.Length != 23)
-        {
-            MessageBox.Show("Xisob raqamni to'liq kiriting iltimos.");
-            return;
-        }
-        customerCreationDto.BankAccount = RemoveDashes(txtYattXisobRaqam.Text);
-        
-        customerCreationDto.BankName = txtYattBank.Text.ToLower();
-        customerCreationDto.YurAddress = txtYattFirmaAdres.Text.ToLower();
-        customerCreationDto.Phone = txtYattTelefon.Text;
-        
-
-
-        if (userJshshir.Equals(""))
-        {
-            MessageBox.Show("YaTT rahbarini ma'lumotni kiriting.");
-            return;
-        }
-        var existUser = await this.userService.RetrieveByJSHSHIRAsync(userJshshir);
-        customerCreationDto.UserId = existUser.Id;
-
-        if (customerCreationDto.Name.Equals("") ||
-            customerCreationDto.Phone.Equals("") ||
-            customerCreationDto.MFO.Equals("") ||
-            customerCreationDto.BankName.Equals("") ||
-            customerCreationDto.YurAddress.Equals(""))
-            MessageBox.Show("Ma'lumotni to'liq kiriting!");
-        else
-        {
-            var result = this.customerService.AddAsync(customerCreationDto);
-
-            if (!result.IsCompletedSuccessfully)
-            {
-                MessageBox.Show($" Saqlandi.");
-                IsCreated = true;
-            }
-            else
-                MessageBox.Show($"{"Saqlashda xatolik"}");
-        }
-    }
 
     // qo'shimcha funsiya
     // xisob raqamni - larni olib tashlaydigan function
     public string RemoveDashes(string input)
         => new string(input.Where(char.IsDigit).ToArray());
-   
-        //TextBoxlar
+
+    //TextBoxlar
     private async void txtJisJSHSHIR_TextChanged(object sender, TextChangedEventArgs e)
     {
         var textBox = sender as TextBox;
@@ -365,7 +75,7 @@ public partial class CustomerAddWindow : Window
                 txtIsmi.Text = existUser.FirstName.ToUpper();
                 txtSharifi.Text = existUser.Patronomyc.ToUpper();
                 txtPasportSeria.Text = existUser.SeriaPasport;
-                txtJisAdres.Text =existUser.Address.ToUpper();
+                txtJisAdres.Text = existUser.Address.ToUpper();
                 txtJisTelefon.Text = existUser.Phone;
                 txtJisTelegramRaqam.Text = existUser.TelegramPhone;
 
