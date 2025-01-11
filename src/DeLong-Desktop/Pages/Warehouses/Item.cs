7f4870b5428471756674daea4dfa75c136c4d825
Avazbek0704 <siddikovavazbek213@gmail.com>
2025-01-11 09:48:21 +0500

    added warehousePage

---
+++ src/DeLong-Desktop/Pages/Warehouses/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace DeLong_Desktop.Pages.Warehouses;
+
+class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ManagerName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
