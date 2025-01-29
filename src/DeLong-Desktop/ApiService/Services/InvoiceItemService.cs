4822859ec678cb473d4f8e9bc351e9dff3bfb219
Avazbek0704 <siddikovavazbek213@gmail.com>
2025-01-29 21:42:55 +0500

    changed services

--- src/DeLong-Desktop/ApiService/Services/InvoiceItemService.cs
+++ src/DeLong-Desktop/ApiService/Services/InvoiceItemService.cs
@@ -2,65 +2,66 @@
 using System.Net.Http;
 using Newtonsoft.Json;
 using DeLong_Desktop.ApiService.Interfaces;
-using DeLong_Desktop.ApiService.DTOs.Category;
 using DeLong_Desktop.ApiService.Models.Commons;
+using DeLong_Desktop.ApiService.DTOs.InvoiceItems;
 
 namespace DeLong_Desktop.ApiService.Services;
 
-class CategoryService : ICategoryService
+class InvoiceItemService : IInvoiceItemService
 {
     private readonly HttpClient _httpClient;
-    public CategoryService()
+    public InvoiceItemService()
     {
         _httpClient = new HttpClient
         {
             BaseAddress = new Uri("http://localhost:5208/") // API URL manzilini o'rnating
         };
     }
-    public async ValueTask<bool> AddAsync(CategoryCreationDto dto)
+
+    public async ValueTask<bool> AddAsync(InvoiceItemCreationDto dto)
     {
         var jsonContent = JsonConvert.SerializeObject(dto);
         var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-        var response = await _httpClient.PostAsync("api/Category/create", content);
+        var response = await _httpClient.PostAsync("api/InvoiceItem/create", content);
         response.EnsureSuccessStatusCode();
         return response.IsSuccessStatusCode;
     }
 
-    public async ValueTask<bool> ModifyAsync(CategoryUpdateDto dto)
+    public async ValueTask<bool> ModifyAsync(InvoiceItemUpdateDto dto)
     {
         var jsonContent = JsonConvert.SerializeObject(dto);
         var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-        var response = await _httpClient.PutAsync("api/Category/update", content);
+        var response = await _httpClient.PutAsync("api/InvoiceItem/update", content);
         response.EnsureSuccessStatusCode();
         return response.IsSuccessStatusCode;
     }
 
     public async ValueTask<bool> RemoveAsync(long id)
     {
-        var response = await _httpClient.DeleteAsync($"api/Category/delete/{id}");
+        var response = await _httpClient.DeleteAsync($"api/InvoiceItem/delete/{id}");
         response.EnsureSuccessStatusCode();
         return response.IsSuccessStatusCode;
     }
-    public async ValueTask<CategoryResultDto> RetrieveByIdAsync(long id)
+
+    public async ValueTask<InvoiceItemResultDto> RetrieveByIdAsync(long id)
     {
-        var response = await _httpClient.GetAsync($"api/Category/get/{id}");
+        var response = await _httpClient.GetAsync($"api/InvoiceItem/get/{id}");
         response.EnsureSuccessStatusCode();
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Response<CategoryResultDto>>(jsonResponse);
+        var result = JsonConvert.DeserializeObject<Response<InvoiceItemResultDto>>(jsonResponse);
         return result.Data;
     }
-
-    public async ValueTask<IEnumerable<CategoryResultDto>> RetrieveAllAsync()
+    
+    public async ValueTask<IEnumerable<InvoiceItemResultDto>> RetrieveAllAsync()
     {
-        var response = await _httpClient.GetAsync("api/Category/get-all");
+        var response = await _httpClient.GetAsync("api/InvoiceItem/get-all");
         response.EnsureSuccessStatusCode();
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Response<List<CategoryResultDto>>>(jsonResponse);
+        var result = JsonConvert.DeserializeObject<Response<List<InvoiceItemResultDto>>>(jsonResponse);
         return result.Data;
     }
-
 }
--- src/DeLong-Desktop/ApiService/Services/InvoiceService.cs
+++ src/DeLong-Desktop/ApiService/Services/InvoiceService.cs
@@ -1,32 +1,68 @@
-﻿using DeLong_Desktop.ApiService.Interfaces;
+﻿using System.Net.Http;
+using DeLong_Desktop.ApiService.Interfaces;
 using DeLong_Desktop.ApiService.DTOs.Invoices;
+using Newtonsoft.Json;
+using System.Text;
+using DeLong_Desktop.ApiService.DTOs.Customers;
+using DeLong_Desktop.ApiService.Models.Commons;
 
 namespace DeLong_Desktop.ApiService.Services;
 
 class InvoiceService : IInvoiceService
 {
-    public ValueTask<bool> AddAsync(InvoiceCreationDto dto)
+    private readonly HttpClient _httpClient;
+    public InvoiceService()
     {
-        throw new NotImplementedException();
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5208/") // API URL manzilini o'rnating
+        };
     }
 
-    public ValueTask<bool> ModifyAsync(InvoiceUpdateDto dto)
+    public async ValueTask<bool> AddAsync(InvoiceCreationDto dto)
     {
-        throw new NotImplementedException();
+        var jsonContent = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("api/Invoice/create", content);
+        response.EnsureSuccessStatusCode();
+        return response.IsSuccessStatusCode;
     }
 
-    public ValueTask<bool> RemoveAsync(long id)
+    public async ValueTask<bool> ModifyAsync(InvoiceUpdateDto dto)
     {
-        throw new NotImplementedException();
+        var jsonContent = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PutAsync("api/Invoice/update", content);
+        response.EnsureSuccessStatusCode();
+        return response.IsSuccessStatusCode;
     }
 
-    public ValueTask<IEnumerable<InvoiceResultDto>> RetrieveAllAsync()
+    public async ValueTask<bool> RemoveAsync(long id)
     {
-        throw new NotImplementedException();
+        var response = await _httpClient.DeleteAsync($"api/Invoice/delete/{id}");
+        response.EnsureSuccessStatusCode();
+        return response.IsSuccessStatusCode;
+    }
+ 
+    public async ValueTask<InvoiceResultDto> RetrieveByIdAsync(long id)
+    {
+        var response = await _httpClient.GetAsync($"api/Invoice/get/{id}");
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<Response<InvoiceResultDto>>(jsonResponse);
+        return result.Data;
     }
 
-    public ValueTask<InvoiceResultDto> RetrieveByIdAsync(long id)
+    public async ValueTask<IEnumerable<InvoiceResultDto>> RetrieveAllAsync()
     {
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync("api/Invoice/get-all");
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<Response<List<InvoiceResultDto>>>(jsonResponse);
+        return result.Data;
     }
 }
