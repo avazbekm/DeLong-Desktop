ae557ad373c9101577c3392ecfe18c55ef505fca
Mansurjon Moydinov <mansurjonmoydinov16072002@gmail.com>
2025-03-21 11:43:21 +0500

    ReportPage

---
+++ src/DeLong-Desktop/Pages/Reports/ReportPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DeLong_Desktop.Pages.Reports
+{
+    public partial class ReportPage : Page
+    {
+        public ReportPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ExportExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ExportPdfButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+       
+    }
+
+    
+}
