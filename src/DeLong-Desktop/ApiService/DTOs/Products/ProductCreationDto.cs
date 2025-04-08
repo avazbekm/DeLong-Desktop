72bad2d80d4d95fb155834c53218a6c2435e5c34
Avazbek0704 <siddikovavazbek213@gmail.com>
2025-04-08 10:52:48 +0500

    changed ProductDtos

--- src/DeLong-Desktop/ApiService/DTOs/Products/ProductCreationDto.cs
+++ src/DeLong-Desktop/ApiService/DTOs/Products/ProductCreationDto.cs
@@ -3,7 +3,7 @@
 public class ProductCreationDto
 {
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string ProductSign { get; set; } = string.Empty;
     public decimal? MinStockLevel { get; set; }  // minimal qoldiqni belgilab qo'yish
     public long CategoryId { get; set; }
     public bool IsActive { get; set; }
--- src/DeLong-Desktop/ApiService/DTOs/Products/ProductUpdateDto.cs
+++ src/DeLong-Desktop/ApiService/DTOs/Products/ProductUpdateDto.cs
@@ -4,7 +4,7 @@ public class ProductUpdateDto
 {
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string ProductSign { get; set; } = string.Empty;
     public decimal? MinStockLevel { get; set; }  // minimal qoldiqni belgilab qo'yish
     public long CategoryId { get; set; }
     public bool IsActive { get; set; }
